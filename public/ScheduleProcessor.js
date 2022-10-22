101df7779efed7d6f35ab0e60eb75e0c89951242
MichaelLaffan1 <michaellaff10876@gmail.com>
2022-10-22 15:02:59 -0400

    Michael's Files

---
+++ public/ScheduleProcessor.js
@@ -0,0 +1,55 @@
+const form = document.querySelector('#classform')
+
+const fname = document.getElementById("fname")
+const lname = document.getElementById("lname")
+const email = document.getElementById("email")
+const schedule = document.getElementById("schedule")
+
+const button = document.getElementById("send")
+
+button.onclick = function(){
+    console.log("Clicked!")
+    console.log(fname.value)
+    console.log(lname.value)
+    console.log(email.value)
+    console.log(schedule.value)
+
+    fetch("/submit", {
+        method: "POST",
+        cache: "no-cache",
+        headers: [["Content-Type", "application/json"]],
+        body: JSON.stringify({
+            fname: fname.value,
+            lname: lname.value,
+            email: email.value,
+            schedule: schedule.value
+        })
+    });
+}
+
+
+/* listen for submit even
+form.addEventListener('submit', event => {
+   // disable default action
+   event.preventDefault()
+
+   // configure a request
+   const xhr = new XMLHttpRequest()
+   xhr.open('POST', '/classes')
+ 
+   // prepare form data
+   let data = new FormData(form)
+ 
+   // set headers
+   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+   xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
+ 
+   // send request
+   xhr.send(data)
+ 
+   // listen for `load` event
+   xhr.onload = () => {
+     console.log(xhr.responseText)
+   }
+})
+*/
