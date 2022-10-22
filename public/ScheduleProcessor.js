53dff070939453271c40e4a2d307af523265af08
MichaelLaffan1 <michaellaff10876@gmail.com>
2022-10-22 18:48:48 -0400

    Implemented File Upload Functionality

--- public/ScheduleProcessor.js
+++ public/ScheduleProcessor.js
@@ -1,17 +1,24 @@
+var dt = new Date();
+document.getElementById('date-time').innerHTML=dt;
 const fname = document.getElementById("fname")
 const lname = document.getElementById("lname")
 const email = document.getElementById("email")
 const schedule = document.getElementById("schedule")
+const image = document.getElementById("schedulephoto")
 
 const button = document.getElementById("send")
 
 button.onclick = function(){
+    if ((fname.value == "") || (lname.value == "") || (email.value == ""))
+    {
+        return "Can't accept empty input";
+    }
     console.log("Clicked!")
     console.log(fname.value)
     console.log(lname.value)
     console.log(email.value)
     console.log(schedule.value)
-
+    console.log(image.value)
     fetch("/submit", {
         method: "POST",
         cache: "no-cache",
@@ -20,7 +27,8 @@ button.onclick = function(){
             fname: fname.value,
             lname: lname.value,
             email: email.value,
-            schedule: schedule.value
+            schedule: schedule.value,
+            image: image.value
         })
     });
     document.getElementById("classform").innerHTML = "Your responses have been submitted. Please await an email from us.";
